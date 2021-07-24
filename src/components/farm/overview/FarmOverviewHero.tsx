b25a184b8934587105b2105bf816eb9aa6e7be6f
entero.vipun <entero.vipunen@protonmail.com>
2021-07-23 22:39:21 -0500

    updated graphics

--- src/components/farm/overview/FarmOverviewHero.tsx
+++ src/components/farm/overview/FarmOverviewHero.tsx
@@ -4,12 +4,14 @@ import { useHistory } from 'react-router-dom';
 import { Button, Divider, Grid, Typography } from '@material-ui/core';
 import styled from 'styled-components';
 import { CardHero, Link } from '@dogechia/core';
-import heroSrc from './images/hero.svg';
+import heroSrc from './images/duck-hunt.svg';
 import PlotAddDirectoryDialog from '../../plot/PlotAddDirectoryDialog';
 import useOpenDialog from '../../../hooks/useOpenDialog';
 
 const StyledImage = styled('img')`
-  max-width: 7rem;
+  max-width: 777px;
+  width: 454px;
+  height: 112px;
 `;
 
 export default function FarmOverviewHero() {
